c14755b27304dd4318d1a39f6865f0308f558c7a
kunaldoshi98 <68844308+kunaldoshi98@users.noreply.github.com>
2020-10-26 17:28:31 +0530

    try

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
-
+import ListItem from "@material-ui/core/ListItem";
 class App extends Component {
   render() {
     const relatives = ["Relative1", "Relative2", "Relative3"];
     return (
       <>
         <ol>
-          {relatives.map((city, index) => (
-            <li key={`location${index + 1}`}>{relatives}</li>
+          {relatives.map((relativeName, index) => (
+            <ListItem key={`relatives${index + 1}`}>{relatives}</ListItem>
           ))}
         </ol>
       </>
